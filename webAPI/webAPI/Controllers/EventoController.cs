667a3cd87b23f397815aba37716973f418d37b40
DESKTOP-U7V5H6P\usuario <snait7@gmail.com>
2020-11-13 10:56:38 +0100

    arreglo cosas

--- webAPI/webAPI/Controllers/EventoController.cs
+++ webAPI/webAPI/Controllers/EventoController.cs
@@ -8,20 +8,10 @@ using webAPI.Models;
 
 namespace webAPI.Controllers
 {
-    [Route("api/Evento/{action}")]
     public class EventoController : ApiController
     {
         // GET: api/Evento
-        [HttpGet]
-        [ActionName("Get")]
-        public IEnumerable<Evento> Get()
-        {
-            EventoRepository repository = new EventoRepository();
-            List<Evento> eventos = repository.retrieve();
-            return eventos;
-        }
-        [HttpGet]
-        [ActionName("GetDTO")]
+
         public IEnumerable<EventoDTO> GetDTO()
         {
             var repository = new EventoRepository();
